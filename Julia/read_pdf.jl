609111731d3540d7a3a5df57c7abcdc1748c2c76
Dirk Oliver Theis <dotheisatutdotee@gmail.com>
2017-04-11 17:53:33 +0300

    Added module for reading pdf from file

---
+++ Julia/read_pdf.jl
@@ -0,0 +1,34 @@
+#read_pdf.jl
+module Read_PDF
+export read_pdf
+
+using JSON
+
+function to_tuple(X) :: Any
+    if typeof(X) <: Array
+        tmp = [ to_tuple(x) for x in X ]
+        x = (tmp[1:end]...)
+    else
+        return X
+    end
+end
+
+
+function array_to_dict(a) :: Dict{Tuple{Any,Any,Any}, Float64}
+    local d = Dict{Tuple{Any,Any,Any}, Float64}()
+    for p in a
+        t = Tuple{Any,Any,Any}( to_tuple( p[1] ) )
+        @show t, typeof(t), p[2]
+        d[t] = Float64( p[2] )
+    end
+    return d
+end
+
+
+function read_pdf(filename::String) ::  Dict{Tuple{Any,Any,Any}, Float64}
+    a = JSON.parsefile(filename)
+    return array_to_dict(a)
+end
+
+
+end
