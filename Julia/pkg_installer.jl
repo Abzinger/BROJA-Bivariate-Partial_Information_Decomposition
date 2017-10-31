5dd1d2b24118579298340b64f670bc18bb2a240e
Abzinger <abdullah.makkeh@gmail.com>
2017-10-31 12:31:37 +0200

    dependencies

--- Julia/pkg_installer.jl
+++ Julia/pkg_installer.jl
@@ -1,14 +1,14 @@
 # all the packages used in Julia 
 Pkg.update()
+Pkg.add("MathProgBase")
 Pkg.add("Mosek")
-Pkg.add("MathProgBaseMosek")
-Pkg.add("MathOptInterfaceMosek")
 Pkg.add("ECOS")
+Pkg.add("SCS")
 Pkg.add("Ipopt")
 Pkg.add("KNITRO")
 Pkg.add("NLopt")
-Pkg.add("SCS")
 Pkg.add("Cuba")
 Pkg.add("CPUTime")
-Pkg.add("MathProgBase")
+Pkg.add("JSON")
+Pkg.update()
 #EOF
